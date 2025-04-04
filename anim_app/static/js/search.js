8892563890de9ebf2ea1f7c4896f2c6f1199138d
EvenGrin_A1002 <evgeniagrigoreva67031@gmail.com>
2025-04-04 16:26:34 +0300

    работа с видео

---
+++ anim_app/static/js/search.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    let search = $('#search')
+    let search_clear = $('#search_clear')
+    if (search.val()){
+            console.log('21213')
+            search_clear.removeClass('d-none')
+        } else {
+            search_clear.addClass('d-none')
+        }
+    search.on('input', function(){
+        if (search.val()){
+            console.log('21213')
+            search_clear.removeClass('d-none')
+        } else {
+            search_clear.addClass('d-none')
+        }
+    })
+    search_clear.on('click', function(){
+        search.val('')
+    })
+})
